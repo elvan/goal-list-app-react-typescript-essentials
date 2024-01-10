1d2328b1541c8e3150b5126d4b2b8c5dbdd3bd9a
Elvan Hidayat <elvanhd@gmail.com>
2024-01-10 22:17:19 +0700

    Refactor App component to include CourseGoal
    - Modify App component to render CourseGoal component
    - Create CourseGoal component for displaying course goals
    - Accept title and description as props in CourseGoal
    - Add delete button to CourseGoal for future functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import CourseGoal from './components/CourseGoal.tsx';
+
 export default function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <CourseGoal
+        title='Learn React + TS'
+        description='Learn it from the ground up'
+      />
+    </main>
+  );
 }
