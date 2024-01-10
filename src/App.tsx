8d5278e0d8b601568b828b8540d6327b9443b80a
Elvan Hidayat <elvanhd@gmail.com>
2024-01-10 23:51:55 +0700

    Introduce NewGoal component for goal input
    - Add NewGoal component for user input of goals and summaries
    - Modify App to utilize NewGoal for adding dynamic goals

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import goalsImg from './assets/goals.jpg';
 import CourseGoalList from './components/CourseGoalList.tsx';
 import Header from './components/Header.tsx';
+import NewGoal from './components/NewGoal.tsx';
 
 export type CourseGoal = {
   title: string;
@@ -13,12 +14,12 @@ export type CourseGoal = {
 export default function App() {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
 
-  function handleAddGoal() {
+  function handleAddGoal(goal: string, summary: string) {
     setGoals((prevGoals) => {
       const newGoal: CourseGoal = {
         id: Math.random(),
-        title: 'Learn React + TS',
-        description: 'Learn it in depth!',
+        title: goal,
+        description: summary,
       };
       return [...prevGoals, newGoal];
     });
@@ -33,7 +34,7 @@ export default function App() {
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <button onClick={handleAddGoal}>Add Goal</button>
+      <NewGoal onAddGoal={handleAddGoal} />
       <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
