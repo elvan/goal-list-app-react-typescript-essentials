68ef4e2189a01108ecaa967963747bdf63ae21fa
Elvan Hidayat <elvanhd@gmail.com>
2024-01-10 23:15:38 +0700

    Implement dynamic goal addition
    - Introduce state for managing course goals dynamically
    - Implement a function to add new goals on button click
    - Update App to render a list of goals with dynamic content
    - Enhance code structure and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
+import { useState } from 'react';
+
 import goalsImg from './assets/goals.jpg';
 import CourseGoal from './components/CourseGoal.tsx';
 import Header from './components/Header.tsx';
 
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!',
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title='Learn React + TS'>
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
